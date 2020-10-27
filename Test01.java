6d8796d3df0bdc7c954ccbd3314fb021d586b981
Bewith34 <58575487+Bewith34@users.noreply.github.com>
2020-10-27 15:38:58 +0000

    Add files via upload
    Initial Commit

---
+++ Test01.java
@@ -0,0 +1,51 @@
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+
+class Test01
+{
+
+	public static void main(String[] args)
+	{
+
+		int largestGap = 0;
+		int gapStart = Integer.MIN_VALUE;
+		// int num = 9;
+		// int num = 362;
+		// int num = 33;
+		// int num = 257;
+		// int num = 17;
+		// int num = 21;
+		// int num = 451;
+		int num = 681;
+		for (int i = Integer.SIZE - 1; i >= 0; i--)
+		{
+			System.out.print(getBit(num, i));
+			if (getBit(num, i) == 1)
+			{
+
+				if (gapStart == Integer.MIN_VALUE)
+				{
+					gapStart = i;
+				} else
+				{
+					if (largestGap < gapStart - i)
+					{
+						largestGap = gapStart - i - 1;
+
+					}
+					gapStart = i;
+				}
+			}
+
+		}
+		System.out.print("\nLargestGap = " + largestGap);
+
+	}
+
+	//Not my method
+	static int getBit(int num, int pos)
+	{
+		return (num >> pos) & 1;
+	}
+
+}
